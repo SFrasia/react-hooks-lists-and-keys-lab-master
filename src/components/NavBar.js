19484405b133ab04d1d06c2ea5e2753d94d9695c
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-02-29 18:43:08 +0300

    Changes

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+  return (
+    <nav>
+      {links.map((link, ind) => (
+        <a href={"#" + link} key={ind}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/components/ProjectList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+
+function ProjectList({ projects }) {
+  console.log(projects);
+  let projecList = projects.map((project) => {
+    const { id, name, about, technologies } = project;
+    return (
+      <ProjectItem
+        key={id}
+        name={name}
+        about={about}
+        technologies={technologies}
+      />
+    );
+  });
+  return (
+    <div id="projects">
+      <h2>My Projects</h2>
+      <div id="project-list">{projecList}</div>
+    </div>
+  );
+}
+
+export default ProjectList;
